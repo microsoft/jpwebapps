7db586056c19b3cb1233e456abfa11c109013133
ychikazawa <ychikazawa@microsoft.com>
2024-02-28 20:23:26 +0900

    Fix post-preview-url-to-pr.ps1

--- pipelines/stages/post-preview-url-to-pr.ps1
+++ pipelines/stages/post-preview-url-to-pr.ps1
@@ -9,7 +9,7 @@ $repositoryId = "1a248aad-a3b5-48fe-8410-8a31c4008db9"
 $siteUrl = "https://happy-bush-0e8a37100-${pullRequestId}.eastasia.4.azurestaticapps.net/"
 
 # Create API Url
-$url = "${organizationUrl}${project}/_apis/git/repositories/${repositoryId}/pullrequests/${pullRequestId}/threads?api-version=7.1-preview.1"
+$url = "${organizationUrl}/${project}/_apis/git/repositories/${repositoryId}/pullrequests/${pullRequestId}/threads?api-version=7.1-preview.1"
 
 # Create Header
 $headers = @{
@@ -18,12 +18,18 @@ $headers = @{
 }
 
 # Create description
-$dscription = "---\n### [system message]\nYour preview site is ready! Visit it here: ${siteUrl}"
+$comment = "Your preview site is ready! Visit it here: ${siteUrl}"
 
 # Create request body
 $body = @{
-    "description" = $description
+    comments= @(
+        @{
+            parentCommentId = 0
+            content = $comment
+            commentType = "system"
+        }
+    )
 }
 
 # Call API
-$response = Invoke-RestMethod -Uri $url -Method Patch -Body ($body | ConvertTo-Json) -Headers $headers
+$response = Invoke-RestMethod -Uri $url -Method Post -Body ($body | ConvertTo-Json) -Headers $headers
